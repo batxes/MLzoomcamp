ed164cb38aefd973bbd755d2cac2d63ea9a3e68d
batxes <batxes@gmail.com>
2023-09-25 23:43:52 +0200

    02-homework fixed

--- 02-regression/02-homework.py
+++ 02-regression/02-homework.py
@@ -4,20 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('housing.csv',sep=",")
-print (df)
 
 #sns.histplot(df.median_house_value, bins=50)
 #plt.show()
 df = df.query('ocean_proximity == "<1H OCEAN" or ocean_proximity == "INLAND"')
 print (df.ocean_proximity.nunique())
-print (df)
 df = df[["latitude","longitude","housing_median_age","total_rooms","total_bedrooms","population","households","median_income","median_house_value"]]
-print (df)
 
-#question1
+print ("\n\n\n")
+print ("### Q1")
 print (df.isnull().sum())
 
-#question2
+print ("\n\n\n")
+print ("### Q2")
 print (np.median(df.population))
 
 np.random.seed(42)
@@ -42,8 +41,8 @@ y_train = np.log1p(df_train.median_house_value.values)
 y_val = np.log1p(df_val.median_house_value.values)
 y_test = np.log1p(df_test.median_house_value.values)
 
-# question 3
-print ("QUESTION 3")
+print ("\n\n\n")
+print ("### Q3")
 
 def train_linear_regression(X, y):
     X = np.array(X) 
@@ -57,7 +56,7 @@ def train_linear_regression(X, y):
 base = df.columns.values.tolist()
 X_train = df_train[base].fillna(0).values
 w0, w = train_linear_regression (X_train,y_train)
-y_pred = w0 + X_train.dot(w)
+y_pred = w0 + df_val.dot(w)
 
 def rmse (y, y_pred):
     error = y - y_pred
@@ -65,16 +64,17 @@ def rmse (y, y_pred):
     mse = se.mean()
     return np.sqrt(mse)
 
-print (rmse(y_train, y_pred))
+print (rmse(y_val, y_pred))
 
 X_train = df_train[base].fillna(df_train.median_house_value.mean()).values
 w0, w = train_linear_regression (X_train,y_train)
-y_pred = w0 + X_train.dot(w)
-print (rmse(y_train, y_pred))
-
+y_pred = w0 + df_val.dot(w)
+print (rmse(y_val,y_pred))
 print ("\n\n\n")
 print ("### Q4")
 X_train = df_train[base].fillna(0).values
+X_val = df_val[base].fillna(0).values
+X_test = df_test[base].fillna(0).values
 
 def train_linear_regression_reg(X, y, r=0.001):
     X = np.array(X) # makes list of list intro matrix
@@ -87,13 +87,73 @@ def train_linear_regression_reg(X, y, r=0.001):
     return w_full[0], w_full[1:]
 
 
-for r in [0.0, 0.000001, 0.0001, 0.001]:
+for r in [0.0, 0.000001, 0.0001, 0.001, 1000]:
     w0, w = train_linear_regression_reg(X_train, y_train, r)
-    y_pred = w0 + X_train.dot(w)
-    score = rmse(y_train, y_pred)
-    print (r, w0, score.round(5))
+    y_pred = w0 + X_val.dot(w)
+    score = rmse(y_val, y_pred)
+    print (r, w0, round(score,2))
 
+print ("\n\n\n")
+print ("### Q5")
 
+scores = []
+for seed in np.arange(10):
+    idx = np.arange(n)
+    np.random.seed(seed)
+    np.random.shuffle(idx)                     
+    df_train = df.iloc[idx[:n_train]]
+    df_val = df.iloc[idx[n_train:n_train+n_val]]
+    df_test = df.iloc[idx[n_train+n_val:]]
+
+    df_train = df_train.reset_index(drop=True)
+    df_val = df_val.reset_index(drop=True)
+    df_test = df_test.reset_index(drop=True)
+
+    X_train = df_train[base].fillna(0).values
+    X_val = df_val[base].fillna(0).values
+    X_test = df_test[base].fillna(0).values
+
+    y_train = np.log1p(df_train.median_house_value.values)
+    y_val = np.log1p(df_val.median_house_value.values)
+    y_test = np.log1p(df_test.median_house_value.values)
+
+    base = df.columns.values.tolist()
+    w0, w = train_linear_regression (X_train,y_train)
+    y_pred = w0 + X_val.dot(w)
+    score = rmse(y_val, y_pred)
+    scores.append(score)
+print (scores)
+print (round(np.std(scores),3))
+
+print ("\n\n\n")
+print ("### Q6")
+
+idx = np.arange(n)
+np.random.seed(9)
+np.random.shuffle(idx)                     
+df_train = df.iloc[idx[:n_train]]
+df_val = df.iloc[idx[n_train:n_train+n_val]]
+df_test = df.iloc[idx[n_train+n_val:]]
+df_full_train = pd.concat([df_train, df_val])                         
+
+df_train = df_train.reset_index(drop = True)
+df_val = df_val.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+df_full_train = df_full_train.reset_index(drop = True)
+
+X_val = df_val[base].fillna(0).values
+X_test = df_test[base].fillna(0).values
+X_full_train = df_full_train[base].fillna(0).values
+
+y_train = np.log1p(df_train.median_house_value.values)
+y_val = np.log1p(df_val.median_house_value.values)
+y_test = np.log1p(df_test.median_house_value.values)
+y_full_train = np.concatenate([y_train, y_val])
+
+w0, w = train_linear_regression_reg(X_full_train, y_full_train, r=0.001)
+y_pred = w0 + X_test.dot(w)
+score = rmse(y_test,y_pred)
+print (score)
 
 
 
