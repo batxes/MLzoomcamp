bb90e054c8de3cc91ec8a3db314e1de17fcc9ef4
batxes <batxes@gmail.com>
2023-10-01 00:22:55 +0200

    progress until 3.5

--- 03-classification/03-classification.py
+++ 03-classification/03-classification.py
@@ -38,6 +38,41 @@ print (df.churn)
 print ("\n\n\n")
 print ("### 3.3 Setting up the validation framework")
 
+from sklearn.model_selection import train_test_split
 
+# train_test_split? # this shows docs in notebooks
 
+df_full_train, df_test = train_test_split(df, test_size=0.2, random_state=1) # this splits into test and train, but we also want to do validation, so we split again, and 20% of 80% is actually 25%
+print (len(df_full_train),len(df_test))
+df_train, df_val = train_test_split(df_full_train, test_size=0.25, random_state=1)
+print (len(df_train),len(df_test),len(df_val))
+df_train = df_train.reset_index(drop=True)
+df_full_train = df_full_train.reset_index(drop=True)
+df_val = df_val.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+y_train = df_train.churn.values
+y_test = df_test.churn.values
+y_val = df_val.churn.values
+del df_train['churn']
+del df_test['churn']
+del df_val['churn']
 
+print ("\n\n\n")
+print ("### 3.4 EDA, exploratory data analysis")
+print (df_full_train.isnull().sum())
+print (df_full_train.churn.value_counts(normalize=True))
+print ("Global churn rate is how many people churn, which is 0.26. We can also get to that with the mean")
+print (df_full_train.churn.mean())
+global_churn_rate = df_full_train.churn.mean()
+
+print (df_full_train.dtypes)
+print ("There are 3 numerical values: tenure, monthly charges and total charges.")
+numerical = ["tenure","monthlycharges","totalcharges"]
+categorical = ['customerid', 'gender', 'seniorcitizen', 'partner', 'dependents', 'phoneservice', 'multiplelines', 'internetservice',
+       'onlinesecurity', 'onlinebackup', 'deviceprotection', 'techsupport',
+       'streamingtv', 'streamingmovies', 'contract', 'paperlessbilling',
+       'paymentmethod']
+print (df_full_train[categorical].nunique())
+
+print ("\n\n\n")
+print ("### 3.5 Feature importance: Churn rate and risk ratio")
